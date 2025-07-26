b8e054d1c8836ebbf4880dcbfd88f9b30ab391ce
Adrian Perez <adrianperezosorio15@gmail.com>
2025-07-26 16:36:07 -0400

    cambios minimos

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,15 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module "*.css" {
-  const content: string;
-  export default content;
-}
-
-declare module "*.scss" {
-  const content: string;
-  export default content;
-}
-
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string
 }
