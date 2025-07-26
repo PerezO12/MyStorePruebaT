e5bbc06d950df324ac83e4b85ee47f5a28465af6
Adrian Perez <adrianperezosorio15@gmail.com>
2025-07-26 16:29:09 -0400

    cambios minimos

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+        }
+      }
+    }
+  }
 })
